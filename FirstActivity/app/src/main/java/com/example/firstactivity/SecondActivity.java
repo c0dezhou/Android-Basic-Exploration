7c277024d05fd2030940b7d84fe81b3a2aa65ad6
zhoux <xiao_y_zhou@163.com>
2019-09-11 13:51:11 +0800

    commit my first andriod test

---
+++ FirstActivity/app/src/main/java/com/example/firstactivity/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.firstactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Intent intent=getIntent();
+        String data=intent.getStringExtra("extra_data");
+        Log.d("SecondActivity",data);
+
+    }
+}
