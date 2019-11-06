f8229f5c07fbc9f8490051e6e32afd48158c3166
zhoux <xiao_y_zhou@163.com>
2019-11-06 15:16:23 +0800

    增加例句朗读功能

--- Word/app/src/main/java/com/example/word/DetailActivity.java
+++ Word/app/src/main/java/com/example/word/DetailActivity.java
@@ -31,12 +31,13 @@ public class DetailActivity extends AppCompatActivity {
 
         final TextView wordWord=(TextView) findViewById(R.id.word_Word);
         final String word=intent.getStringExtra("wordWord");
+        final String sample=intent.getStringExtra("wordSample");
         wordWord.setText(intent.getStringExtra("wordWord"));
 
         TextView wordExplain=(TextView) findViewById(R.id.word_Explain);
         wordExplain.setText("解释："+intent.getStringExtra("wordExplain"));
 
-        TextView wordSample=(TextView) findViewById(R.id.word_Sample);
+        final TextView wordSample=(TextView) findViewById(R.id.word_Sample);
         wordSample.setText("示例："+intent.getStringExtra("wordSample"));
 
         final Button btn_voice=(Button) findViewById(R.id.btn_voice);
@@ -58,6 +59,52 @@ public class DetailActivity extends AppCompatActivity {
                 // 设置指定的流媒体地址
                 try {
                     mediaPlayer.setDataSource(url + word);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // 设置音频流的类型
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                // 通过异步的方式装载媒体资源
+                mediaPlayer.prepareAsync();
+                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        // 装载完毕 开始播放流媒体
+                        mediaPlayer.start();
+                        Toast.makeText(DetailActivity.this, "开始播放", Toast.LENGTH_SHORT).show();
+                        // 避免重复播放，把播放按钮设置为不可用
+                        //btn_voice.setEnabled(false);
+                    }
+
+
+                    //startActivity(intent);
+
+                });
+
+
+            }
+
+        });
+
+        final Button btn_Samplevoice=(Button) findViewById(R.id.btn_Samplevoice);
+        btn_Samplevoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://dict.youdao.com/dictvoice?audio=";
+//                Intent intent = new Intent();
+//                //显示数据给用户
+//                intent.setAction(Intent.ACTION_VIEW);
+//                //设置为浏览器类型
+//                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+//
+//                //设置要访问的网址
+//                Uri uri = Uri.parse(url+word);
+//                intent.setData(uri);
+
+                final MediaPlayer mediaPlayer = new MediaPlayer();
+                // 设置指定的流媒体地址
+                try {
+                    mediaPlayer.setDataSource(url + sample);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
